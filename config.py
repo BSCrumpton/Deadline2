ae4be66bb5da66a7a31ace0b2b206a3bcb188f70
Saul Crumpton <saul292.atmes@yahoo.com>
2021-05-02 00:42:10 -0400

    Form rework & option group fix (#13)
    * adding in DeadlineForm
    
    currently works to add and delete deadlines
    
    * removing edit button for now
    
    this will be added back in the future.
    
    * renamed the form to ConfigForm
    
    will add a new deadline form
    
    * updated calendar form to also use ui file
    
    * Cal-Form now auto adjusts size, has minimum
    
    * moved process deadline to bottom right instead of bottom left
    
    * fixed the issue with resetting options groups settings from default upon deadline creation
    
    * updating readme, changelog and versions
    
    also removed paypal link of original gist creator
    
    * Update zipper.sh
    
    added a space after manifest .json so that the zipper would actually pick it up.
    
    Co-authored-by: Crumpton, Bryan S <saul.crumpton@gatech.edu>

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Anki Deadline2
 # Anki 2.1 plugin
 # Author: BSC
-# Version 2_2_1
+# Version 2_3
 # Description: Adjusts 'New Cards per Day' setting of options group to ensure all cards
 #              are seen by deadline.
 # License: GNU GPL v3 <www.gnu.org/licenses/gpl.html>
@@ -10,6 +10,7 @@ import datetime, time, math
 from PyQt5.QtWidgets import *
 from anki.hooks import wrap, addHook
 from aqt import *
+from . import ConfigForm, CalForm
 from aqt.main import AnkiQt
 from anki.utils import intTime
 from aqt.utils import showWarning, openHelp, getOnlyText, askUser, showInfo, openLink
@@ -22,46 +23,26 @@ class DeadlineDialog(QDialog):
         self.mw = aqt.mw
         self.deadlines = mw.addonManager.getConfig(__name__)
         self.deadlines.pop("test")
-        self.form = aqt.forms.fields.Ui_Dialog()
+        self.form = ConfigForm.Ui_Dialog()
         self.form.setupUi(self)
         self.setWindowTitle(_("Deadline") )
-        self.form.buttonBox.button(QDialogButtonBox.Help).setAutoDefault(False)
-        self.form.buttonBox.button(QDialogButtonBox.Save).setAutoDefault(False)
-        self.form.buttonBox.button(QDialogButtonBox.Save).setText('Process Deadlines')
-        self.form.buttonBox.button(QDialogButtonBox.Save).clicked.connect(self.callDeadlines)
+        self.form.ProcessDeadlineBox.clicked.connect(self.callDeadlines)
         self.fillFields()
         self.setupSignals()
-        self.form.fieldList.setCurrentRow(0)
-        self.form.rtl.setParent(None)
-        self.form.fontFamily.setParent(None)
-        self.form.fontSize.setParent(None)
-        self.form.sticky.setParent(None)
-        self.form.label_18.setParent(None)
-        self.form.fontFamily.setParent(None)
-        self.form.fieldRename.setParent(None)
-        self.form.fieldPosition.setParent(None)
-        self.form.label_5.setParent(None)
-        self.form.sortField.setParent(None)
-        self.popUpLayout=QHBoxLayout()
-        self.popUpLabel = QLabel()
-        self.popUpLabel.setText("Choose Pop-Up Style:")
-        self.popUpBox = QComboBox()
-        self.popUpBox.addItem("Single Summary Pop Up")
-        self.popUpBox.addItem("One Pop Up per Deck")
         if(self.deadlines.get("oneOrMany","")=="Many"):
-            self.popUpBox.setCurrentIndex(1)
+            self.form.OneOrManyBox.setCurrentIndex(1)
         else:
-            self.popUpBox.setCurrentIndex(0)
-        self.popUpLayout.addWidget(self.popUpLabel)
-        self.popUpLayout.addWidget(self.popUpBox)
-        self.form.verticalLayout.addLayout(self.popUpLayout)
+            self.form.OneOrManyBox.setCurrentIndex(0)
         self.resize(500, 500)
-
+        self.Calwindow=QDialog(self)
+        self.LayoutForCal=CalForm.Ui_Dialog()
+        self.LayoutForCal.setupUi(self.Calwindow)
+        self.LayoutForCal.pushButton.clicked.connect(self.readValues)
         self.exec_()
 
     def callDeadlines(self):
         from . import manualDeadlines
-        tempString=str(self.popUpBox.currentText())
+        tempString=str(self.form.OneOrManyBox.currentText())
         if(tempString.find("Single")!=-1):
             self.deadlines["oneOrMany"]="One"
         else:
@@ -81,25 +62,25 @@ class DeadlineDialog(QDialog):
 
     def setupSignals(self):
         f = self.form
-        f.fieldAdd.clicked.connect(self.onAdd)
-        f.fieldDelete.clicked.connect(self.onDelete)
+        f.AddDeadlineButton.clicked.connect(self.onAdd)
+        f.DeleteDeadlineButton.clicked.connect(self.onDelete)
         f.buttonBox.helpRequested.connect(self.onHelp)
 
     def readValues(self):
         user=str(aqt.mw.pm.name)
-        deck=str(self.deckBox.currentText())
-        year=self.cal.selectedDate().year()
-        month=self.cal.selectedDate().month()
-        day=self.cal.selectedDate().day()
+        deck=str(self.LayoutForCal.comboBox.currentText())
+        year=self.LayoutForCal.calendarWidget.selectedDate().year()
+        month=self.LayoutForCal.calendarWidget.selectedDate().month()
+        day=self.LayoutForCal.calendarWidget.selectedDate().day()
         date="{}-{}-{}".format(year,str(month).zfill(2),str(day).zfill(2))
         self.user=user
         self.deck=deck
         self.date=date
-        self.window.close()
+        self.Calwindow.close()
         if(not self.user in self.deadlines["deadlines"]):
             self.deadlines["deadlines"][self.user]={}
         self.deadlines["deadlines"][self.user][self.deck]=self.date
-        tempString=str(self.popUpBox.currentText())
+        tempString=str(self.form.OneOrManyBox.currentText())
         if(tempString.find("Single")!=-1):
             self.deadlines["oneOrMany"]="One"
         else:
@@ -111,70 +92,44 @@ class DeadlineDialog(QDialog):
         mw.addonManager.writeConfig(__name__, self.deadlines)
         dconf = mw.col.decks.all_config()
         tempID=0
+        DeckIDToUpdate= mw.col.decks.id_for_name(deck)
         for conf in dconf:
+            # if the specialized config already exists, use that
             if(conf['name']==deck):
                 tempID=conf['id']
         if(tempID==0):
-            tempID = mw.col.decks.confId(deck)
-        deckToUpdate=mw.col.decks.byName(deck)
+            # else, create the new config
+            # TODO: use the new function names to do this
+            tempID = mw.col.decks.confId(deck,mw.col.decks.confForDid(DeckIDToUpdate))
+        deckToUpdate = mw.col.decks.byName(deck)
         deckToUpdate['conf']=tempID
         mw.col.decks.save(deckToUpdate)
 
     def onAdd(self):
-        self.user=""
-        self.window=QDialog(self)
-        self.window.setWindowTitle("Add new Deadline")
-        self.window.resize(500,500)
-        dropdownLayout=QVBoxLayout()
-        dropdownLayout.addStretch()
-        dropdownLabel=QLabel()
-        dropdownLabel2=QLabel()
-        dropdownLabel.setText("Select the deck you want to add a deadline to:")
-        dropdownLabel2.setText("Note: The dealine is when you will see all NEW cards by")
-        dropdownLayout.addWidget(dropdownLabel)
-        layout=QHBoxLayout()
-        self.cal=QCalendarWidget()
-        self.cal.setGridVisible(True)
-        self.cal.move(0, 0)
-        self.cal.setGeometry(100,100,300,300)
-        self.deckBox = QComboBox()
+        self.Calwindow.show()
+        self.user = ""
+        self.LayoutForCal.comboBox.clear()
         for deck in sorted(aqt.mw.col.decks.allNames()):
             deckId=mw.col.decks.byName(deck)['id']
             new_cards = mw.col.db.scalar("""select count() from cards where type = 0 and queue != -1 and did = ?""", deckId)
             if(new_cards<1):
                 continue
-            self.deckBox.addItem(deck)
-        dropdownLayout.addWidget(self.deckBox)
-        dropdownLayout.addWidget(dropdownLabel2)
-        dropdownLayout.addStretch()
-        layout.addLayout(dropdownLayout)
-        calLayout=QVBoxLayout()
-        calLabel=QLabel()
-        calLabel.setText("Select your deadline date:")
-        calLayout.addWidget(calLabel)
-        calLayout.addWidget(self.cal)
-        layout.addLayout(calLayout)
-        self.okButton = QDialogButtonBox.Ok
-        self.buttonBox=QDialogButtonBox(self.okButton)
-        self.buttonBox.button(self.okButton).clicked.connect(self.readValues)
-        layout.addWidget(self.buttonBox)
-        self.window.setLayout(layout)
-        self.window.show()        
+            self.LayoutForCal.comboBox.addItem(deck)
+
 
     def onDelete(self):
-        toDelete=self.form.fieldList.currentRow()
-        if(toDelete==-1):
-            return
-        temp=self.form.fieldList.item(toDelete).text()
-        fields=temp.split("}")
-        user=fields[0].split("{")[1]
-        deck=fields[1].split("{")[1]
-        date=fields[2].split("{")[1]
-        self.deadlines["deadlines"].get(user).pop(deck)
-        self.fillFields()
-        mw.addonManager.writeConfig(__name__, self.deadlines)
-        delConfId=mw.col.decks.byName(deck)['conf']
-        mw.col.decks.remConf(delConfId)
+        while self.form.fieldList.selectedIndexes():
+            temp=self.form.fieldList.item(self.form.fieldList.selectedIndexes()[0].row()).text()
+            self.form.fieldList.takeItem(self.form.fieldList.selectedIndexes()[0].row())
+            fields=temp.split("}")
+            user=fields[0].split("{")[1]
+            deck=fields[1].split("{")[1]
+            date=fields[2].split("{")[1]
+            self.deadlines["deadlines"].get(user).pop(deck)
+            mw.addonManager.writeConfig(__name__, self.deadlines)
+            delConfId=mw.col.decks.byName(deck)['conf']
+            mw.col.decks.remConf(delConfId)
+        # self.fillFields()
 
     def onHelp(self):
         openLink('https://github.com/BSCrumpton/Deadline2')
